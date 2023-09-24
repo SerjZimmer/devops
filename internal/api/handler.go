64b80e837708cc25a59b366cdbf367b696ce7f4e
SerjZimmer <rusakov.sd@aobfg.ru>
2023-09-24 19:30:16 +0300

    test

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -42,6 +42,8 @@ func NewHandler(stor metricsStorage) *Handler {
 	}
 }
 
+//
+
 func (s *Handler) LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		startTime := time.Now()
