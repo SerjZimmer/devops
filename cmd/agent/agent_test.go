cba26cba1306c2c385ff3ead2746f54922e6f71c
SerjZimmer <rusakov.sd@aobfg.ru>
2023-09-30 17:04:28 +0300

    test refactor

--- cmd/agent/agent_test.go
+++ cmd/agent/agent_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/SerjZimmer/devops/internal/storage"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,9 +10,10 @@ import (
 // Mock sendMetric function for testing
 var (
 	originalSendMetric = sendMetric
-	sendMetricT        = func(s *storage.MetricsStorage, address string) {}
+	sendMetricT        = func(m storage.Metrics, address string) {}
 )
 var strg = storage.NewMetricsStorage()
+var m storage.Metrics
 
 func Test_sendMetric(t *testing.T) {
 	// Create a test HTTP server
@@ -26,13 +26,13 @@ func Test_sendMetric(t *testing.T) {
 	defer func() { sendMetricT = originalSendMetric }()
 
 	// Test sendMetric with test data
-	strg.Metrics.MType = "gauge"
-	strg.Metrics.ID = "metricName"
+	m.MType = "gauge"
+	m.ID = "metricName"
 	v := 123.45
-	strg.Metrics.Value = &v
+	m.Value = &v
 
 	// Call sendMetric
-	sendMetric(strg, "localhost:8080")
+	sendMetric(m, "localhost:8080")
 }
 
 func Test_sendMetricCounter(t *testing.T) {
@@ -46,30 +46,12 @@ func Test_sendMetricCounter(t *testing.T) {
 	defer func() { sendMetricT = originalSendMetric }()
 
 	// Test sendMetric with test data
-	strg.Metrics.MType = "counter"
-	strg.Metrics.ID = "metricName"
+	m.MType = "counter"
+	m.ID = "metricName"
 	v := 123
 	vi := int64(v)
-	strg.Metrics.Delta = &vi
+	m.Delta = &vi
 
 	// Call sendMetric
-	sendMetric(strg, "localhost:8080")
-}
-
-func Test_monitoring(t *testing.T) {
-	// Create a test HTTP server
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
-	defer server.Close()
-
-	// Replace the sendMetric function with a mock for testing
-	sendMetricT = func(s *storage.MetricsStorage, address string) {
-		// Mock behavior for sendMetric function during testing
-		// You can add assertions or checks here as needed
-		assert.Equal(t, "gauge", strg.Metrics.MType)
-		assert.Equal(t, "metricName", strg.Metrics.ID)
-		// Check metricValue against expected values
-	}
-	go monitoring(strg, "localhost:8080", 10, 10)
+	sendMetric(m, "localhost:8080")
 }
