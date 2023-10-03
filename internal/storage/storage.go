c8f6009589cb550deecd8661dfa5cc96d48d1945
SerjZimmer <rusakov.sd@aobfg.ru>
2023-10-04 00:43:04 +0300

    test

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -39,7 +39,8 @@ func NewMetricsStorage(c *config.Config) *MetricsStorage {
 			for i := range t.C {
 				err := m.writeToDisk()
 				if err != nil {
-					fmt.Println(err, i)
+					fmt.Println(i)
+					fmt.Println(err)
 				}
 			}
 		}
